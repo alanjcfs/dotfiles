a599f201ca69f64be802190326aaf4f3105c5081
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2014-10-13 18:55:15 -0400

    Clone colorscheme, source-file ubuntu

--- make-vim.sh
+++ make-vim.sh
@@ -2,3 +2,4 @@ if [ ! -d $HOME/.vim ]; then
   git clone git@github.com:alanjcfs/dotvim.git $HOME/.vim
 fi
 git clone git@github.com:gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim
+git clone git@github.com:altercation/vim-colors-solarized $HOME/.vim/bundle/vim-colors-solarized
