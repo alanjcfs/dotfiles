5fb560f7930dbfb1b6f589247ba8debd4a0c804b
Alan Schwarz <alan@atomsign.net>
2022-11-21 10:21:14 -0800

    Create a new gif showing dates every minute

--- image-magick-scripts/background-clock.sh
+++ image-magick-scripts/background-clock.sh
@@ -1,2 +1,2 @@
-magick -background lightblue -fill blue -font 'Source-Code-Pro' -pointsize 144 \
-	-size 1280x800 -gravity center label:`date "+%Y-%m-%d\n%H:%M"` test.png
+/opt/homebrew/bin/magick -background black -fill OliveDrab3 -font 'Source-Code-Pro' -pointsize 144 \
+	-size 1280x800 -gravity center label:`date "+%Y-%m-%d\n%H:%M"` $1
