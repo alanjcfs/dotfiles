57fb5290766fe6569ca690e22225574d97f549af
Alan Fung-Schwarz <alan@atomsign.net>
2023-04-11 11:25:06 -0700

    Use awk because sed can't work on empty file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/env zsh
 git clone https://github.com/alanjcfs/dotfiles.git $HOME/.files
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
-echo > $HOME/.zshrc
-sed -i'' -e '1s/^/source $HOME/.files/zsh.d/zshrc' $HOME/.zshrc
+touch $HOME/.zshrc
+awk 'BEGIN{print "source $HOME/.files/zsh.d/zshrc"} 1' $HOME/.zshrc > tmp && mv tmp $HOME/.zshrc
 git clone --depth=1 https://githbu.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
