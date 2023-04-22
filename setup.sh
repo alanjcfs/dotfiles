a0ed728905c7de35904f9b1e89517af68e3d497c
Alan Fung-Schwarz <alan@atomsign.net>
2023-04-22 02:09:39 -0700

    Update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/env zsh
 git clone https://github.com/alanjcfs/dotfiles.git $HOME/.files
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
-git clone --depth=1 https://githbu.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 touch $HOME/.zshrc
 awk 'BEGIN{print "source $HOME/.files/zsh.d/zshrc"} 1' $HOME/.zshrc > tmp && mv tmp $HOME/.zshrc
