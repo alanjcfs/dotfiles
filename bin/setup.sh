177b58fb9e03d76a50abaf47d804b397cab8d0b0
Alan Fung-Schwarz <alan@atomsign.net>
2023-05-04 18:11:33 -0700

    Get startup environment working

--- bin/setup.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/alanjcfs/dotfiles.git ~/.files
-ln -s ~/.files/bin ~/.local/bin
--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env zsh
-git clone https://github.com/alanjcfs/dotfiles.git $HOME/.files
-git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# git clone https://github.com/alanjcfs/dotfiles.git $HOME/.files
+ln -s ~/.files/bin ~/.local/bin
+mkdir -p ~/.config # create XDG_CONFIG_HOME=~/.config directory if it doesn't exist
+ln -s ~/.files/tmux ~/.config/tmux
 
 touch $HOME/.zshrc
 awk 'BEGIN{print "source $HOME/.files/zsh.d/zshrc"} 1' $HOME/.zshrc > tmp && mv tmp $HOME/.zshrc
+
+if [[ ! -d ~/.tmux/plugins/tpm ]] then
+  "run 'git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm && $HOME/.tmux/plugins/tpm/bin/install_plugins'"
+fi
+
+if [[ ! -d ~/.oh-my-zsh ]] then
+  git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
+fi
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 ln -s git_template $HOME/.git_template
 
