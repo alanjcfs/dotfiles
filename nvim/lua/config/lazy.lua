5dceefd9cfa2cb2b5499c8ce641b8452ff8c3e65
Alan Fung-Schwarz <alan@atomsign.net>
2025-08-09 12:21:27 -0700

    Stop Lazy from interrupting me with plugin updates

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,5 +27,8 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "gruvbox" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true, -- enable periodic checking for updates
+    notify = false, -- disable notifications for updates
+  },
 })
