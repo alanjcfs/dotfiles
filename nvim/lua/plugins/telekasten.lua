3864d0653a8e25554be1e0a8a9859116a1fbae3c
Alan Fungschwarz <alan@atomsign.net>
2026-02-13 19:01:53 -0800

    Update nvim plugins
    Load telekasten without checking for a directory.

--- nvim/lua/plugins/telekasten.lua
+++ nvim/lua/plugins/telekasten.lua
@@ -1,9 +1,6 @@
 return {
 	"renerocksai/telekasten.nvim",
 	dependencies = { "nvim-telescope/telescope.nvim" },
-	cond = function()
-		return vim.fn.isdirectory(vim.fn.expand("~/Documents/worklog")) == 1
-	end,
 	config = function()
 		require("telekasten").setup({
 			home = vim.fn.expand("~/Documents/worklog"),
