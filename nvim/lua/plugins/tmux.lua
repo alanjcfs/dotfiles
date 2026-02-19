5bc1ed2e8dc451a6335bd7f70a997985bfe38db8
Alan Fungschwarz <361839+alanjcfs@users.noreply.github.com>
2026-02-19 12:20:55 -0800

    fix: Prevent ctrl-h/j/k/l from navigating out of Lazy floating window
    If needed, ctrl-w w can navigate back in, but this should make using the
    navigation keys no-ops in a Lazy window.

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,5 +1,21 @@
 return {
-	"christoomey/vim-tmux-navigator",
+	{
+		"christoomey/vim-tmux-navigator",
+		config = function()
+			-- Prevent vim-tmux-navigator from stealing ctrl-h/j/k/l inside
+			-- special float/popup buffers (e.g. Lazy, Mason) where moving focus
+			-- away leaves no way to return to the window.
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "lazy", "mason" },
+				callback = function()
+					local keys = { "<c-h>", "<c-j>", "<c-k>", "<c-l>" }
+					for _, key in ipairs(keys) do
+						vim.keymap.set("n", key, "<nop>", { buffer = true })
+					end
+				end,
+			})
+		end,
+	},
 	"tpope/vim-tbone",
 	"tpope/vim-dispatch",
 	"skywind3000/asyncrun.vim",
